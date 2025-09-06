270126db7716c2ccfdfc42216dfe38880996f30a
leo <xzc55214881@gmail.com>
2025-09-06 20:13:00 +0900

    ...整合mqtt...

--- pig-common/pig-common-core/src/main/java/com/pig4cloud/pig/common/core/service/MqttPublisher.java
+++ pig-common/pig-common-core/src/main/java/com/pig4cloud/pig/common/core/service/MqttPublisher.java
@@ -31,4 +31,26 @@ public class MqttPublisher {
     public void broadcast(String payload) {
         send("pig/common", payload);
     }
+
+    /**
+     * 1. retain = true（保留消息）
+     * Broker 会 保存这条消息为该 topic 的最新消息。
+     * 新订阅者一连接就能立即收到这条消息，而不需要等待新的发布。
+     * 常用场景：状态消息（例如用户余额、设备开关状态、最新价格）。
+     * 特点：
+     * Broker 内只保存 每个 topic 的最后一条消息。
+     * 同一 topic 的新消息会覆盖旧消息。
+     * 2. retain = false（普通消息）
+     * Broker 不保存消息，只推送给当时在线的订阅者。
+     * 离线的订阅者不会收到这条消息。
+     * 常用场景：事件消息（例如交易账单、通知、日志）。
+     */
+    public void sendRetain(String topic, String payload, Boolean retain) {
+        mqttHandler.handleMessage(
+                MessageBuilder.withPayload(payload)
+                        .setHeader("mqtt_topic", topic)
+                        .setHeader("mqtt_retain", retain)
+                        .build()
+        );
+    }
 }
