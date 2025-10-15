3356531df7c71d7f5e3a665d27592819417d3171
leo <xzc55214881@gmail.com>
2025-10-15 18:50:12 +0900

    ...请假审批流程测试...

---
+++ pig-workflow/pig-workflow-biz/src/main/java/com/pig4cloud/pig/workflow/mapper/LeaveRequestMapper.java
@@ -0,0 +1,11 @@
+package com.pig4cloud.pig.workflow.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.pig4cloud.pig.workflow.api.entity.LeaveRequest;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface LeaveRequestMapper extends BaseMapper<LeaveRequest> {
+
+}
